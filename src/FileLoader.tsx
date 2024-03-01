0c8b797c674ef535cee06117714f660a32076b5a
Nikita <nikita.vitkovsky@icloud.com>
2024-03-01 18:07:44 +0000

    import fix

--- src/FileLoader.tsx
+++ src/FileLoader.tsx
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Storage from '@aws-amplify/storage';
-
-
+import { Storage } from 'aws-amplify';
 
 const FileUploader = () => {
   const [files, setFiles] = useState([]);
